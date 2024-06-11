93caef6f7ddd83ea951fa879f265d019ac4bce92
Francisco Valdés <ecodisonante@gmail.com>
2024-06-10 22:39:06 -0400

    pagina detalle juegos

--- src/app/dashboard/pages/game/game.component.ts
+++ src/app/dashboard/pages/game/game.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Game } from '../../../models/game';
+import { GAMES } from '../../../models/database';
 
 @Component({
   selector: 'app-game',
@@ -7,6 +10,18 @@ import { Component } from '@angular/core';
   templateUrl: './game.component.html',
   styleUrl: './game.component.css'
 })
-export default class GameComponent {
+export default class GameComponent implements OnInit {
+  public game?: Game;
 
+  constructor(private route: ActivatedRoute, private router: Router) { }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      const id = params.get('id');
+
+      if (id && !isNaN(Number(id))) this.game = GAMES.find(g => g.id === Number(id));
+      else this.router.navigate(['/']);
+
+    });
+  }
 }
