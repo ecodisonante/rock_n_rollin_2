4a5f9549ead4fb446af425689aaeace68e3f6513
Francisco Valdés <ecodisonante@gmail.com>
2024-06-10 22:19:22 -0400

    cambio rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,35 +2,41 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     {
+        path: 'categories',
+        title: 'Categorías',
+        loadComponent: () => import('./dashboard/pages/categories/categories.component')
+    },
+    {
+        path: 'user',
+        title: 'Registrar Usuario',
+        loadComponent: () => import('./dashboard/pages/user/user.component')
+    },
+    {
+        path: 'category/:id',
+        title: 'Juegos Categoría ',
+        loadComponent: () => import('./dashboard/pages/category/category.component')
+    },
+    {
+        path: 'game/:id',
+        loadComponent: () => import('./dashboard/pages/game/game.component')
+    },
+    {
+        path: 'categories',
+        title: 'Categorías',
+        loadComponent: () => import('./dashboard/pages/categories/categories.component')
+    },
+    {
         path: '',
-        loadComponent: () => import('./dashboard/dashboard.component'),
-        children: [
-            {
-                path: 'categories',
-                title: 'Categorías',
-                loadComponent: () => import('./dashboard/pages/categories/categories.component')
-            },
-            {
-                path: 'user',
-                title: 'Registrar Usuario',
-                loadComponent: () => import('./dashboard/pages/user/user.component')
-            },
-            {
-                path: 'category/:id',
-                title: 'Juegos Categoría ',
-                loadComponent: () => import('./dashboard/pages/category/category.component')
-            },
-            {
-                path: 'game/:id',
-                loadComponent: () => import('./dashboard/pages/game/game.component')
-            },
-            {
-                path: 'categories',
-                title: 'Categorías',
-                loadComponent: () => import('./dashboard/pages/categories/categories.component')
-            },
-        ]
+        redirectTo: 'categories',
+        pathMatch: 'full'
+    },
+    { // Momentaneamente el home es la pagina categorias
+        path: 'home',
+        redirectTo: 'categories',
+        pathMatch: 'full'
     }
+
+
 
 
 ];
